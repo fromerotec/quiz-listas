a00349a0b8a8544c7b11450827e34ac005d908d9
fromerotec <fernandoromero@tec.mx>
2021-09-27 00:59:22 -0700

    Commit 2

--- assignments/01QuizListas/src/exercise.py
+++ assignments/01QuizListas/src/exercise.py
@@ -1,26 +1,6 @@
 def main():
     #escribe tu código abajo de esta línea
-    lista1 = []
-    lista2 = []
-    lista3 = []
-
-    num = int(input())
-
-    if num <= 0:
-        print("Error")
-    else:
-        for i in range(num):
-            lista1.append(int(input()))
-        
-        for i in range(num):
-            lista2.append(int(input()))
-
-        for i in range(num):
-            lista3.append(lista1[i] + lista2[i])
-        
-        print(lista3)
-
-
+    pass
 
 if __name__=='__main__':
     main()
